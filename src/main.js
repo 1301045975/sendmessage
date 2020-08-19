7240a048157e47ed56d564694fee3bc86da68212
Zhaohui <zhaohui_zh@foxmail.com>
2020-08-19 19:20:24 +0800

    路由权限控制

--- src/main.js
+++ src/main.js
@@ -3,12 +3,35 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ElementUI from "element-ui";
+import { Message, MessageBox } from 'element-ui'
 import "element-ui/lib/theme-chalk/index.css";
 
 Vue.use(ElementUI);
 Vue.use(router);
 
 Vue.config.productionTip = false;
+
+// 
+// 未登录时，通过url访问需要登录后才能访问的页面，跳转到首页
+// 
+router.beforeEach((to, from, next) => {
+  if (to.meta.needLogin) {
+    if (store.getters.token) {
+      next();
+    } else {
+      Message({
+        message: '您未登录，无法访问此页面。请先登录',
+        type: 'warning',
+        duration: 3 * 1000
+      });
+      next({
+        path: "/"
+      })
+    }
+  } else {
+    next();
+  }
+})
 
 new Vue({
   router,
