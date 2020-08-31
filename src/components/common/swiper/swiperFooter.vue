05e103d923d69b9e901023ed92acd80d61219a0c
strugt <strugt@foxmail.com>
2020-08-31 20:58:17 +0800

    添加图片轮播

--- src/components/common/swiper/swiperFooter.vue
+++ src/components/common/swiper/swiperFooter.vue
@@ -1,11 +1,15 @@
 <template>
-    <section class="wh_footer_content">
-        <div :class="className" class="wh_footer_swiper">
-            <el-button @click="prevSlide">&lt;</el-button>
-            <div class="wh_footer_swiper">
+    <section :class="className" class="wh_footer_content">
+        <div class="wh_footer_swiper_root">
+            <div class="wh_pre_btn">
+                <slot name="prevBtn"></slot>
+            </div>
+            <div class="wh_next_btn">
+                <slot name="nextBtn"></slot>
+            </div>
+            <div class="wh_footer_swiper_items">
                 <slot />
             </div>
-            <el-button @click="nextSlide">&gt;</el-button>
         </div>
     </section>
 </template>
@@ -16,19 +20,142 @@ export default {
     data() {
         return {
             slidesLength: 1,
-            width: 0,
+            // 所有的图片
+            slideDom: null,
+            // 每张图片的大小
+            imgWidth: 0,
+            // 视野像素大小
+            eyeWidth: 0,
+            // 视野中所能容下的图片总数
+            eyeNumImg: 0,
             className: "",
             dom: {},
-            index: 1
+            // 当前展示的是哪张图片
+            index: 0,
+            // 第几张图片为中间的图片
+            // left | mid | right
+            // 由此可以算出left = mid - 1
+            // right = eyeNumImg - mid
+            mid: 0,
+            left: 0,
+            right: 0,
+            timer1: '',
         };
     },
-    props: {},
+    props: {
+        //滑动所需要的时间
+        autoPlay: {
+            default: true
+        },
+        //一次滑动需要走多久
+        duration: {
+            default: 500
+        },
+        //两次滑动间隔的时间
+        interval: {
+            default: 2500
+        }
+    },
+    mounted() {
+        this.className = `wh_footer_swiper_${Math.random().toFixed(3) * 1000}`;
+        // dom可能还没有更新，所以用setTimeout
+        setTimeout(() => {
+            // 初始化
+            this.starDom();
+            if (this.autoPlay) {
+                this.setTime();
+            }
+        }, 50);
+    },
     methods: {
         prevSlide() {
-            //
+            this.index--;
+            this.wh();
         },
         nextSlide() {
-            //
+            this.index++;
+            this.wh();
+        },
+        slideTo(index) {
+            this.index = index;
+            this.wh();
+        },
+        wh(type) {
+            if (this.eyeNumImg < 2) return;
+            if (this.autoPlay) {
+                this.setTime();
+            }
+            this.index = (this.index + this.slidesLength) % this.slidesLength;
+            this.dom.transition =
+                type == "touch" ? "250ms" : this.duration + "ms";
+            // console.log('index: ' + this.index);
+            // console.log('left: ' + this.left);
+            // console.log('right: ' + this.right);
+            // console.log('eyeNumImg: ' + this.eyeNumImg);
+            // 需要判断平移方式
+            let right = this.slidesLength - this.index - 1;
+            let delta = 0;
+            // 可以将选中的图片放在中间(mid处)
+            if (this.index >= this.left && right >= this.right) {
+                delta = this.index - this.left;
+            } else if (this.index >= this.left) {
+                // 选中图片在right处
+                delta = this.slidesLength - this.eyeNumImg;
+            } else {
+                // 选中图片在left处
+                delta = 0;
+            }
+            delta = delta >= 0 ? delta : 0;
+            this.setTransform(delta * this.imgWidth * -1);
+            this.$emit('slideToIndex', this.index);
+        },
+        setTransform(num) {
+            this.dom.transform = `translate3d(${num}px, 0px, 0px)`;
+            this.dom["-webkit-transform"] = `translate3d(${num}px, 0px, 0px)`;
+            this.dom["-ms-transform"] = `translate3d(${num}px, 0px, 0px)`;
+        },
+        starDom() {
+            this.slideDom = document
+                .querySelector("." + this.className)
+                .getElementsByClassName("wh_footer_slide");
+            this.slidesLength = this.slideDom.length;
+
+            let prevBtnWidth = document
+                .querySelector("." + this.className)
+                .getElementsByClassName("wh_pre_btn");
+            if (prevBtnWidth.length > 0 && prevBtnWidth[0].firstChild)
+                prevBtnWidth = prevBtnWidth[0].firstChild.getBoundingClientRect()
+                    .width;
+            else prevBtnWidth = 0;
+            if (this.slidesLength > 1) {
+                // offsetWidth属性将会 round(四舍五入)为一个整数。
+                // https://developer.mozilla.org/zh-CN/docs/Web/API/HTMLElement/offsetWidth
+                // this.width = document.querySelector('.' + this.className).offsetWidth
+                this.imgWidth = this.slideDom[0].getBoundingClientRect().width;
+                this.eyeWidth =
+                    document
+                        .querySelector("." + this.className)
+                        .getBoundingClientRect().width -
+                    prevBtnWidth * 2;
+                this.eyeNumImg = Math.floor(this.eyeWidth / this.imgWidth);
+                this.mid = Math.floor((this.eyeNumImg + 1) / 2);
+                this.left = this.mid - 1;
+                this.right = this.eyeNumImg - this.mid;
+                this.dom = document
+                    .querySelector("." + this.className)
+                    .getElementsByClassName("wh_footer_swiper_items")[0].style;
+            }
+        },
+        setTime() {
+            window.clearTimeout(this.timer1);
+            this.timer1 = window.setTimeout(() => {
+                if (this.autoPlay) {
+                    this.index++;
+                    this.wh();
+                } else {
+                    window.clearTimeout(this.timer1);
+                }
+            }, this.interval);
         }
     }
 };
@@ -42,15 +169,21 @@ export default {
     width: 100%;
 }
 
-.wh_footer_swiper {
+.wh_footer_swiper_root {
+    display: inline-block;
+}
+
+.wh_footer_swiper_items {
     display: -webkit-box;
     display: -moz-box;
     display: -webkit-flex;
     display: -ms-flexbox;
     display: flex;
+    overflow: hidden;
     -moz-transition-duration: 0s linear;
     -webkit-transition-duration: 0s linear;
     -o-transition-duration: 0s linear;
     transition-duration: 0s linear;
+    margin: 0 2.5em 0 2.5em;
 }
 </style>
--- src/views/old/Detail.vue
+++ src/views/old/Detail.vue
@@ -9,7 +9,8 @@
                         <p>这是提示：123</p>
                     </el-col>
                     <el-col :offset="4" :span="4">
-                        <el-button class="favor-house" size="medium">关注房源</el-button><br>
+                        <el-button class="favor-house" size="medium">关注房源</el-button>
+                        <br />
                         <p>100个人关注</p>
                     </el-col>
                 </el-row>
@@ -17,26 +18,45 @@
         </div>
         <div class="box-body">
             <el-row>
-                <el-col :span="16">
-                    军军房产网 > 成都二手房 > 郫都区二手房 > 犀浦二手房 > 当前房源 
-                </el-col>
+                <el-col :span="16">军军房产网 > 成都二手房 > 郫都区二手房 > 犀浦二手房 > 当前房源</el-col>
                 <el-col :span="8">房源发布机构 加入对比 分享此房源</el-col>
             </el-row>
             <el-row>
                 <el-col :span="16">
-                    <swiper ref="swiper" :autoPlay="true" :showIndicator="true">
+                    <swiper ref="swiper" :autoPlay="false" :showIndicator="true">
                         <slide v-for="(imgUrl, index) in imgUrls" :key="'imgUrls' + index">
                             <img class="img-main" :src="imgUrl" />
                         </slide>
                     </swiper>
-                    <swiper-footer ref="swiperFooter">
-                        <swiper-footer-slide v-for="(imgUrl, index) in imgUrls" :key="'imgUrlsFooter' + index">
-                            <img class="img-footer" :src="imgUrl" />
+                    <swiper-footer ref="swiperFooter" :autoPlay="autoPlay" @slideToIndex="handleSlideToIndex">
+                        <div slot="prevBtn" class="prev-btn" @click="handlePrevSlide">&lt;</div>
+                        <swiper-footer-slide
+                            v-for="(imgUrl, index) in imgUrls"
+                            :key="'imgUrlsFooter' + index"
+                            @click="hanleSlideTo(index)"
+                        >
+                            <img
+                                :class="index == imgActiveIndex ? 'img-active img-footer' : 'img-footer'"
+                                :src="imgUrl"
+                            />
                         </swiper-footer-slide>
+                        <div slot="nextBtn" class="next-btn" @click="handleNextSlide">&gt;</div>
                     </swiper-footer>
+                    <h2>基本信息</h2>
+                    <el-divider></el-divider>
+                    <h2>房源特色</h2>
+                    <el-divider></el-divider>
+                    <h2>税收计算器</h2>
+                    <el-divider></el-divider>
+                    <h2>官方认证</h2>
+                    <el-divider></el-divider>
                 </el-col>
                 <el-col :span="8">
-
+                    <div>
+                        <span> 168万 </span>
+                        <span> 19001元/平米 </span>
+                    </div>
+                    <el-divider></el-divider>
                 </el-col>
             </el-row>
         </div>
@@ -47,7 +67,12 @@
 <script>
 import MyHeader from "@/components/common/MyHeader.vue";
 import MyFooter from "@/components/common/MyFooter.vue";
-import {Swiper, Slide, SwiperFooter, SwiperFooterSlide} from "@/components/common/swiper"
+import {
+    Swiper,
+    Slide,
+    SwiperFooter,
+    SwiperFooterSlide
+} from "@/components/common/swiper";
 
 export default {
     name: "OldDetail",
@@ -57,26 +82,49 @@ export default {
         Swiper,
         Slide,
         SwiperFooter,
-        SwiperFooterSlide,
+        SwiperFooterSlide
     },
     data() {
         return {
-            imgUrls: ['https://image1.ljcdn.com/110000-inspection/pc1_vcEpfzgJg_1.jpg.710x400.jpg',
-                    'https://image1.ljcdn.com/110000-inspection/pc0_hTQIjDmR6_1.jpg.710x400.jpg',
-                    'https://image1.ljcdn.com/110000-inspection/pc0_rKiKXKOg7_1.jpg.710x400.jpg',
-                    'https://image1.ljcdn.com/110000-inspection/pc1_vcEpfzgJg_1.jpg.710x400.jpg',
-                    'https://image1.ljcdn.com/110000-inspection/pc0_hTQIjDmR6_1.jpg.710x400.jpg',
-                    'https://image1.ljcdn.com/110000-inspection/pc0_rKiKXKOg7_1.jpg.710x400.jpg',
-                    'https://image1.ljcdn.com/110000-inspection/pc1_vcEpfzgJg_1.jpg.710x400.jpg',
-                    'https://image1.ljcdn.com/110000-inspection/pc0_hTQIjDmR6_1.jpg.710x400.jpg',
-                    'https://image1.ljcdn.com/110000-inspection/pc0_rKiKXKOg7_1.jpg.710x400.jpg']
+            imgActiveIndex: 0,
+            autoPlay: true,
+            imgUrls: [
+                "https://image1.ljcdn.com/110000-inspection/pc1_vcEpfzgJg_1.jpg.710x400.jpg",
+                "https://image1.ljcdn.com/110000-inspection/pc0_hTQIjDmR6_1.jpg.710x400.jpg",
+                "https://image1.ljcdn.com/110000-inspection/pc0_rKiKXKOg7_1.jpg.710x400.jpg",
+                "https://image1.ljcdn.com/110000-inspection/pc1_vcEpfzgJg_1.jpg.710x400.jpg",
+                "https://image1.ljcdn.com/110000-inspection/pc0_hTQIjDmR6_1.jpg.710x400.jpg",
+                "https://image1.ljcdn.com/110000-inspection/pc0_rKiKXKOg7_1.jpg.710x400.jpg",
+                "https://image1.ljcdn.com/110000-inspection/pc1_vcEpfzgJg_1.jpg.710x400.jpg",
+                "https://image1.ljcdn.com/110000-inspection/pc0_hTQIjDmR6_1.jpg.710x400.jpg",
+                "https://image1.ljcdn.com/110000-inspection/pc0_rKiKXKOg7_1.jpg.710x400.jpg"
+            ]
         };
+    },
+    methods: {
+        handlePrevSlide() {
+            this.$refs.swiper.prevSlide();
+            this.$refs.swiperFooter.prevSlide();
+        },
+        handleNextSlide() {
+            this.$refs.swiper.nextSlide();
+            this.$refs.swiperFooter.nextSlide();
+        },
+        hanleSlideTo(index) {
+            this.$refs.swiper.slideTo(index);
+            this.$refs.swiperFooter.slideTo(index);
+            
+        },
+        handleSlideToIndex(index){
+            this.$refs.swiper.slideTo(index);
+            this.imgActiveIndex = index;
+        }
     }
 };
 </script>
 
 <style scoped>
-.favor-house{
+.favor-house {
     height: 4em;
 }
 .cheader {
@@ -91,16 +139,48 @@ export default {
     margin: 0 auto;
     padding: 20px;
 }
-.title{
+.title {
     font-size: 2em;
 }
-.img-main{
+.img-main {
     height: 100%;
     width: 100%;
 }
-.img-footer{
-    height: 6em;
-    width: 10em;
+.img-footer,
+.prev-btn,
+.next-btn {
+    height: 5em;
+    width: 8em;
     margin: 0.5em 0.5em 0 0;
+    cursor: pointer;
+}
+.prev-btn {
+    position: absolute;
+    float: left;
+    left: 0;
+    display: block;
+    width: 2em;
+    cursor: pointer;
+    color: #fff;
+    background-color: #333;
+    margin: 0.5em 0 0 0;
+    z-index: 199;
+}
+
+.next-btn {
+    position: absolute;
+    float: right;
+    right: 0;
+    display: block;
+    width: 2em;
+    cursor: pointer;
+    color: #fff;
+    background-color: #333;
+    margin: 0.5em 0 0 0;
+    z-index: 199;
+}
+
+.img-active {
+    border: 1px solid red;
 }
 </style>
